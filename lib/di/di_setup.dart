a88a08d087db081fba625938400965db47fbcd44
YeJun, Jung <yejun614@naver.com>
2024-07-12 19:33:15 +0900

    feat: swm datasource, repository를 di에 등록

--- lib/di/di_setup.dart
+++ lib/di/di_setup.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_server_driven_ui/data/accommodation/accommodation_repository.dart';
+import 'package:flutter_server_driven_ui/data/swm/swm_repository.dart';
 import 'package:flutter_server_driven_ui/datasource/accommodation/accommodation_datasource.dart';
+import 'package:flutter_server_driven_ui/datasource/swm/swm_datasource.dart';
 import 'package:flutter_server_driven_ui/presentation/server_driven/adapter_controller/adapter_controller.dart';
 import 'package:get_it/get_it.dart';
 
@@ -12,6 +14,10 @@ void configureDependencies() {
   getIt.registerSingleton<AccommodationRepository>(
     AccommodationRepositoryImpl(),
   );
+  getIt.registerSingleton<SwmDataSource>(
+    SwmDataSourceImpl(),
+  );
+  getIt.registerSingleton<SwmRepository>(SwmRepositoryImpl());
   getIt.registerSingleton<AdapterController>(
     AdapterController(),
   );
