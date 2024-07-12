e23f26e73621e15b5e477c8d664581617b0f3352
JSPark <48265129+pknujsp@users.noreply.github.com>
2024-07-12 18:44:53 +0900

    feat: SwmRepository 생성

---
+++ lib/datasource/swm/swm_datasource.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter_server_driven_ui/datasource/response_model/server_driven_ui/response/v2/server_driven_ui_response_v2.dart';
+import 'package:http/http.dart' as http;
+
+abstract class SwmDataSource {
+  Future<ServerDrivenUIV2Response> getSwmInformation(String url);
+}
+
+class SwmDataSourceImpl implements SwmDataSource {
+  @override
+  Future<ServerDrivenUIV2Response> getSwmInformation(String url) async {
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == HttpStatus.ok) {
+      final body = jsonDecode(response.body);
+      return ServerDrivenUIV2Response.fromJson(body);
+    } else {
+      throw Exception('$url 응답 오류: ${response.statusCode}');
+    }
+  }
+}
